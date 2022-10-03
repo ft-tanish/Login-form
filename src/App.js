0ec7eb12b029461e7fad9e10ceff9aa74b098bc2
Tanish Chaurasia <tanishchaurasia6@gmail.com>
2022-10-03 23:10:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [allEntry, setAllEntry] = useState([]);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const newEntry = { email: email, password: password };
+    setAllEntry([...allEntry, newEntry]);
+    // console.log(allEntry);
+  };
+
+  return (
+    <div className="App">
+      <h2>Login</h2>
+      <form action="" onSubmit={submitForm}>
+        <div className="user-box">
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="user-box">
+          <label htmlFor="password ">Password</label>
+          <input
+            type="text"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button id="btn" type="submit">
+          Login
+        </button>
+      </form>
+      {/* <div>
+        {allEntry.map((curElem) => {
+          return (
+            <div>
+              <p>{curElem.email}</p>
+              <p>{curElem.password}</p>
+            </div>
+          );
+        })}
+      </div> */}
+    </div>
+  );
+}
